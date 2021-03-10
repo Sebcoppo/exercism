51d71c5de8bddd41fccd03b84e168615b5950dab
Sebastien Coppolani <sebcoppo@yahoo.fr>
2021-03-10 08:05:49 +0100

    new exercice completed

---
+++ javascript/leap/leap.js
@@ -0,0 +1,5 @@
+
+export const isLeap = (year) => {
+  // throw new Error('Remove this statement and implement this function');
+    return ((year % 4 == 0) && (year % 100 != 0 )) || (year % 400 == 0);
+};
